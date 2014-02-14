5dfa4f849dc21a3a8629a3dd4dbb0ff296a6eee3
Ivo de Kler <ivo.de.kler@gmail.com>
2014-02-14 15:55:40 +0100

    added animation to background gradients

--- hop/content/randomcolorgradient.js
+++ hop/content/randomcolorgradient.js
@@ -45,6 +45,30 @@ function combineColorsRandomly(passedColors){
 	return newColor;
 }
 
+function combineBuffers(buffer1, buffer2, ratio){
+	if(buffer1.length !== buffer2.length) return 'buffer mismatch';
+	ratio = ratio !== undefined ? ratio : .5;
+	var newBuffer = [],
+		invRatio = 1 - ratio,
+		item1, item2;
+	
+	for(var i in buffer1){
+		item1 = buffer1[i];
+		item2 = buffer2[i];
+
+		newBuffer.push({
+			t: item1.t * invRatio + item2.t * ratio,
+			color: [
+				item1.color[0] * invRatio + item2.color[0] * ratio,
+				item1.color[1] * invRatio + item2.color[1] * ratio,
+				item1.color[2] * invRatio + item2.color[2] * ratio
+			]
+		});
+	}
+
+	return newBuffer;
+}
+
 function colorBufferToString(buffer){
 	var string = '',
 		workBuffer = buffer.slice(),
@@ -64,4 +88,31 @@ function colorBufferToString(buffer){
 	return string;
 }
 
-$(document.body).css({background: 'linear-gradient(to right, ' + colorBufferToString(randomColorBufferA({n: 50, colors: [[0,0,0],[1,0,0],[0,1,0],[0,0,1]]})) + ')'});
+function slowlyChangingBackgroundGradient(options){
+	var buffers = [],
+		colors = [[0,0,0],[1,0,0],[0,1,0],[0,0,1]],
+		t = 0;
+
+	buffers.push(
+		randomColorBufferA({n: 50, colors: colors}),
+		randomColorBufferA({n: 50, colors: colors})
+	);
+	setInterval(function(){
+		if(window['noGradientAnimation']) return;
+		$(document.body).css({
+			background: 'linear-gradient(to right, ' + colorBufferToString(
+				combineBuffers(buffers[0], buffers[1], (Math.cos(t) / -2 + .5))
+			) + ')'
+		});
+		
+		t += .02;
+		if(t >= Math.PI){
+			t -= Math.PI;
+			buffers.shift();
+			buffers.push(randomColorBufferA({n: 50, colors: colors}));
+		}
+	}, 50);
+}
+
+slowlyChangingBackgroundGradient();
+
